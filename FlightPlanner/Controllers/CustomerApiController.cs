124cb1c90e004a26654f993c9fbe4fc4557940eb
IntsFedotovskis <ints.fedotovskis@gmail.com>
2022-02-11 18:34:44 +0200

    1st Iteration

---
+++ FlightPlanner/Controllers/CustomerApiController.cs
@@ -0,0 +1,43 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Mvc;
+using PageResult = FlightPlanner.Models.PageResult;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CustomerApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Airports")]
+        public IActionResult SearchAirports(string search)
+        {
+            var airport = FlightStorage.FindAirports(search);
+            return Ok(airport);
+        }
+
+        [HttpPost]
+        [Route("flights/search")]
+        public IActionResult SearchFlights(SearchFlightRequest request)
+        {
+            if (!FlightRequest.IsValid(request))
+                return BadRequest();
+            return Ok(new PageResult());
+        }
+
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult FindFlightById(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(flight);
+        }
+    }
+}
---
+++ FlightPlanner/Controllers/TestingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FlightPlanner.Storage;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("testing-api")]
+    [ApiController]
+    public class TestingController : ControllerBase
+    {
+        [HttpPost]
+        [Route("clear")]
+        public IActionResult Clear()
+        {
+            FlightStorage.ClearFlights();
+            return Ok();
+        }
+    }
+}
